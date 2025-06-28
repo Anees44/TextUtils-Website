553831368fe0a28bb503ed77177a57b0ef67a607
Anees Ur Rehman <160379828+Anees44@users.noreply.github.com>
2025-06-27 23:00:25 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "TextUtils - App",
+  title: "TextUtils - Website",
   description: "This is the website which is providing you a different facilities like text to uppercase, lowercase, clear, remove extra spaces, copy a text etc",
 };
 
