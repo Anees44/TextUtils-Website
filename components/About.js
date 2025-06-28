ceab7bcaadef2bf91b234b6726b6c4db8d366c5b
Anees Ur Rehman <160379828+Anees44@users.noreply.github.com>
2025-06-27 22:56:01 -0700

    Update About.js

--- components/About.js
+++ components/About.js
@@ -47,17 +47,17 @@ const About = () => {
               </button>
             </h2>
             <div id="collapseOne" className="hidden px-4 py-2" style={myStyle}>
-              <strong>This is the first item's accordion body.</strong> It is
+              <strong>This is the first item&apos;s accordion body.</strong> It is
               shown by default, until the collapse plugin adds the appropriate
               classes that we use to style each element. These classes control
               the overall appearance, as well as the showing and hiding via CSS
               transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the{' '}
-              <code>.accordion-body</code>, though the transition does limit
-              overflow.
+              overriding our default variables. It&apos;s also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
             </div>
           </div>
+
           <div className="border-2 border-black-200 rounded-md" style={myStyle}>
             <h2 className="mb-0">
               <button
@@ -73,17 +73,17 @@ const About = () => {
               </button>
             </h2>
             <div id="collapseTwo" className="hidden px-4 py-2" style={myStyle}>
-              <strong>This is the second item's accordion body.</strong> It is
+              <strong>This is the second item&apos;s accordion body.</strong> It is
               hidden by default, until the collapse plugin adds the appropriate
               classes that we use to style each element. These classes control
               the overall appearance, as well as the showing and hiding via CSS
               transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the{' '}
-              <code>.accordion-body</code>, though the transition does limit
-              overflow.
+              overriding our default variables. It&apos;s also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
             </div>
           </div>
+
           <div className="border-2 border-black-200 rounded-md" style={myStyle}>
             <h2 className="mb-0">
               <button
@@ -99,18 +99,18 @@ const About = () => {
               </button>
             </h2>
             <div id="collapseThree" className="hidden px-4 py-2" style={myStyle}>
-              <strong>This is the third item's accordion body.</strong> It is
+              <strong>This is the third item&apos;s accordion body.</strong> It is
               hidden by default, until the collapse plugin adds the appropriate
               classes that we use to style each element. These classes control
               the overall appearance, as well as the showing and hiding via CSS
               transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the{' '}
-              <code>.accordion-body</code>, though the transition does limit
-              overflow.
+              overriding our default variables. It&apos;s also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
             </div>
           </div>
         </div>
+
         <div className="container my-3">
           <button
             onClick={toggleStyle}
@@ -124,7 +124,5 @@ const About = () => {
     </>
   );
 };
-
-
 
 export default About;
